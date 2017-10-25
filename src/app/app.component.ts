102764a6c75f21f09d30a58f48105f95bd1fdd80
newmann <newmannhu@qq.com>
2017-10-25 17:18:01 +0800

    created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '测试angular2-schema-form';
+
+  // The schema that will be used to generate a form
+  mySchema = {
+    "properties": {
+      "email": {
+        "type": "string",
+        "description": "邮箱",
+        "format": "email"
+      },
+      "password": {
+        "type": "string",
+        "description": "密码",
+        "buttons": [{
+          "id": "reset",
+          "label": "Reset"
+        }]
+      },
+      "rememberMe": {
+        "type": "boolean",
+        "default": false,
+        "description": "记住我"
+      }
+    },
+    "required": ["email", "password", "rememberMe"],
+    "buttons": [{
+      "id": "alert", // the id of the action callback
+      "label": "Alert !" // the text inside the button
+    }]
+  };
+
+  // Declare a mapping between action ids and their event listener
+  myActions = {
+    "alert": (property) => { alert(JSON.stringify(property.value)) },
+    "reset": (property) => { property.reset() }
+  };
+
+  myModel = {email: " john.doe@example.com"};
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {DefaultWidgetRegistry, SchemaFormModule, WidgetRegistry} from './schema-form';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SchemaFormModule
   ],
-  providers: [],
+  providers: [{provide: WidgetRegistry, useClass: DefaultWidgetRegistry}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
